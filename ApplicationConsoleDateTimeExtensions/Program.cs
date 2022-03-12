7b159ae4df5d392186f7bcaef2a72df4f1e732d7
fred <domaine75@gmail.com>
2022-03-12 23:00:53 +0100

    Ajoutez des fichiers projet.

---
+++ ApplicationConsoleDateTimeExtensions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DateTimeExtension;
+
+namespace ApplicationConsoleDateTimeExtensions
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Action<string> Display = Console.WriteLine;
+      Display("DateTime extensions usage sample");
+      DateTime today = DateTime.Today;
+      Display($"The first day of the month is {DateTimeExtensionClass.StartOfMonth(today)}");
+      Display($"The last day of the month is {DateTimeExtensionClass.EndOfMonth(today)}");
+      Display($"The start of the day is {DateTimeExtensionClass.StartOfDay(today)}");
+      Display($"The end of the day is {DateTimeExtensionClass.EndOfDay(today)}");
+
+      Display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
